b11fdcae76f3757dc6927c2aaa3c5f2187088c0a
NydusBorn <nydusborn@outlook.com>
2025-06-06 22:28:50 +0300

    adjust slide numbering, make a more informative ending

--- global-bottom.vue
+++ global-bottom.vue
@@ -4,7 +4,7 @@
 
 <template>
   <footer
-      v-if="$nav.currentLayout !== 'cover'"
+      v-if="$nav.currentLayout !== 'cover' && $nav.currentLayout !== 'fact'"
       class="absolute bottom-0 left-0 right-0 p-2"
   >
     {{ $nav.currentPage }} / {{ $nav.total }}
